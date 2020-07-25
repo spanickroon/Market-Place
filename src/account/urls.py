20e9b82e292c767d482fb96c63d46f84ec9b8dd0
Nikita Koznev <nikitakoznev@gmail.com>
2020-07-25 19:57:10 +0300

    Added path to statics and media

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,6 +1,9 @@
 """This module contain urls for application."""
 
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import test
 
 
@@ -10,3 +13,5 @@ name_apps = 'account'
 urlpatterns = [
     path('', test)
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/config/settings.py
+++ src/config/settings.py
@@ -119,4 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_URL = 'images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'
