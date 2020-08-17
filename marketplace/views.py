46fb1e4d9381f4209f09479ab9e2878e784d0a98
Nikita Koznev <nikitakoznev@gmail.com>
2020-08-17 16:17:10 +0300

    Created a blank profile page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class ProfileView(View):
+    def get(self: object, request: object) -> object:
+        return render(request, template_name='index.html')
