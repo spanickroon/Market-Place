feba4c65098cb8f1bd8a613fd12369998263818c
Nikita Koznev <nikitakoznev@gmail.com>
2020-09-05 17:44:13 +0300

    Created a profile model

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE,
+        verbose_name="User")
+
+    avatar_binary = models.BinaryField(
+        verbose_name="Avatar binary", blank=True, editable=False)
+
+    class Meta:
+        """Meta data."""
+        db_table = 'Users'
+        verbose_name = 'Users'
+        verbose_name_plural = 'Users'
+
+    def __str__(self) -> str:
+        """Funtion for output info about this profile object."""
+        return self.user.username
